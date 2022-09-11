04b63bde2f9005b1874b524645a5509e7acbbbc9
Putseyeu <putseyeu.a.d@gmail.com>
2022-09-11 10:05:00 +0200

    step3

--- Assets/Scripts/BarHealth.cs
+++ Assets/Scripts/BarHealth.cs
@@ -8,14 +8,12 @@ public class BarHealth : MonoBehaviour
     [SerializeField] Slider _healhtBar;
     [SerializeField] private Player _player;
 
-    private void Update()
-    {
-        
-    }
+    private Coroutine _ñhangeValueBar;
 
     public void OnValueChanged(int value, int maxValue)
     {
-        _healhtBar.value = (float)value / maxValue;
+        float valueNew =(float)value / maxValue;
+        _ñhangeValueBar= StartCoroutine(ChangeValueBar(valueNew));
     }
 
     private void OnEnable()
@@ -26,5 +24,17 @@ public class BarHealth : MonoBehaviour
     private void OnDisable()
     {
         _player.ChangedHealth -= OnValueChanged;
+    }
+
+    private IEnumerator ChangeValueBar(float valueNew)
+    {
+        float runningTime = 0;
+
+        while (_healhtBar.value != valueNew)
+        {
+            runningTime += Time.deltaTime;
+            _healhtBar.value = Mathf.MoveTowards(_healhtBar.value, valueNew, runningTime);
+            yield return null;
+        }
     }
 }
